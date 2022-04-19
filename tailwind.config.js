620ed1dcc3405063b080f2d1d6b99cf235bfc953
Max Georgopoulos <73665502+maxgeor@users.noreply.github.com>
2022-04-19 00:08:56 -0400

    First commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        bitter: ['Bitter', 'serif'],
+        dm: ['DM Sans', 'sans'],
+        karla: ['Karla', 'sans'],
+        lora: ['Lora', 'serif'],
+        work: ['Work Sans', 'sans'],
+      },
+    },
+  },
+  plugins: [],
+}
